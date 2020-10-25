c1a7db168135c46c796092408cc0dce760d11241
sjsloreilly <sjsloreilly@gmail.com>
2020-10-24 22:23:46 -0700

    updating optimize.py, unsure if window function is working?

--- optimize.py
+++ optimize.py
@@ -1,6 +1,7 @@
 from pyspark.sql.dataframe import DataFrame
-from pyspark.sql import functions as f, types as t
+from pyspark.sql import functions as f, types as t, Window
 from pyspark.sql import SparkSession
+from datetime import datetime, timedelta
 import requests
 
 taxiPath = "hdfs:///tmp/data/emr/nyc-taxi/taxi-data/output/section2/json/"
@@ -30,47 +31,39 @@ def get_zip_code_mapping_df():
     returnZipDF = zipReadDF.select('Borough', 'Neighborhood', f.explode(f.split('ZIP Codes', ',')).alias('zip'))
     return returnZipDF
 
-    joinAirDF = airQualityDF.withColumn('air_day', f.date_format("dateObserved", "yyyyMMdd")).withColumnRenamed("zip", "air_zip")
-    zipDF = zipDF.withColumn('zipBorough', f.col('Borough'))
-    taxiZipDF = taxiDF.join(zipDF.select('zip', 'ZipBorough'), zipDF.zipBorough == taxiDF.Borough)
-    joinTaxiDF = (taxiZipDF.withColumn("pickup_day", f.date_format("pickup_datetime", "yyyyMMdd"))
-                        .withColumn("pickup_month", f.date_format("pickup_datetime", "yyyyMM")))
-    joinCondition = [joinTaxiDF.pickup_day == joinAirDF.air_day, joinTaxiDF.zip == joinAirDF.air_zip]
-    joinedAirDF = joinTaxiDF.join(joinAirDF.select('categoryNumber', 'air_day', 'air_zip'), joinCondition)
-    aggDF = (joinedAirDF.groupBy('pickup_month', 'pickup_day', 'zip')
-                        .agg(f.count('pickup_day').alias('count_rides'), f.avg('categoryNumber').alias('avg_cat')))
-    
-    win = Window.partitionBy("zip", "pickup_month").orderBy(f.desc("avg_cat", 'count_rides'))
-    aggDF = aggDF.withColumn("row_num", f.row_number().over(win)).where("row_num >= 10")
-
-    return aggDF
-
 def get_air_quality_df(zipDF):
-    zipList = [x[0] for x in zipDF.select('zip').collect()]
+#     zipList = [x[0] for x in zipDF.select('zip').collect()]
+    zipList = ['11212'] #'10023', '11374', '11414']
     airQualitySchema = t.StructType([
-        t.StructField("dateObserved", t.DateType(), True),
-        t.StructField("hourObserved", t.IntegerType(), True),
-        t.StructField("localTimeZone", t.StringType(), True),
-        t.StructField("reportingArea", t.StringType(), True),
-        t.StructField("stateCode", t.StringType(), True),
-        t.StructField("latitude", t.IntegerType(), True),
-        t.StructField("longitude", t.IntegerType(), True),
-        t.StructField("parameterName", t.StringType(), True),
-        t.StructField("aqi", t.IntegerType(), True),
-        t.StructField("categorNumber", t.IntegerType(), True),
-        t.StructField("categoryName", t.StringType(), True),
-        t.StructField("zip", t.IntegerType(), True),
+        t.StructField("AQI", t.LongType(), True),
+        t.StructField("Category", t.MapType(t.StringType(), t.LongType()), True),
+        t.StructField("DateObserved", t.StringType(), True),
+        t.StructField("HourObserved", t.LongType(), True),
+        t.StructField("Latitude", t.DoubleType(), True),
+        t.StructField("LocalTimeZone", t.StringType(), True),
+        t.StructField("Longitude", t.DoubleType(), True),
+        t.StructField("ParameterName", t.StringType(), True),
+        t.StructField("ReportingArea", t.StringType(), True),
+        t.StructField("StateCode", t.StringType(), True),
+        t.StructField("zip", t.StringType(), True),
         ])
     
     airDF = spark.createDataFrame([], airQualitySchema)
 
     for zipCode in zipList:
-        apiPath = f"http://www.airnowapi.org/aq/observation/zipCode/historical/?format=json&zipCode={zipCode}&distance=25&API_KEY=B79C872D-FC7A-4037-BFA6-F064276F2EB3"
-        request = requests.get(apiPath)
-        print(request)
-        print(request.json())
-        requestDF = spark.createDataFrame(request.json(), airQualitySchema).withColumn('zip', f.lit(zipCode))
-        returnAirDF = airDF.unionAll(requestDF)
+        for d in range(0,99):
+            startDate = datetime.strptime("2020-06-01", "%Y-%m-%d").date()
+            dateDelta = timedelta(days=d)
+            endDate = startDate + dateDelta
+            
+            apiPath = f"https://www.airnowapi.org/aq/observation/zipCode/historical/?format=application/json&zipCode={zipCode}&date={endDate}T00-0000&distance=100&API_KEY=8DFC7E6B-F641-41D9-95DC-9CF3B90AF038"
+            request = requests.get(apiPath)
+            requestDF = spark.createDataFrame(request.json())
+            requestDF = requestDF.withColumn('zip', f.lit(zipCode))
+            airDF = airDF.unionAll(requestDF)
+        
+    returnAirDF = airDF.withColumn("categoryNumber", f.col("Category.Number"))
+    returnAirDF.show()
     
     return returnAirDF
 
@@ -81,6 +74,22 @@ def save_data(df, output_path, data_format, write_mode):
     .format(data_format)
     .save(output_path))
     
+def calculate_hottest_days(taxiDF, airQualityDF, zipDF):
+    joinAirDF = airQualityDF.withColumn('air_day', f.date_format("dateObserved", "yyyyMMdd")).withColumnRenamed("zip", "air_zip")
+    zipDF = zipDF.withColumn('zipBorough', f.col('Borough'))
+    taxiZipDF = taxiDF.join(zipDF.select('zip', 'ZipBorough'), zipDF.zipBorough == taxiDF.Borough)
+    joinTaxiDF = (taxiZipDF.withColumn("pickup_day", f.date_format("pickup_datetime", "yyyyMMdd"))
+                        .withColumn("pickup_month", f.date_format("pickup_datetime", "yyyyMM")))
+    joinCondition = [joinTaxiDF.pickup_day == joinAirDF.air_day, joinTaxiDF.zip == joinAirDF.air_zip]
+    joinedAirDF = joinTaxiDF.join(joinAirDF.select('categoryNumber', 'air_day', 'air_zip'), joinCondition)
+    aggDF = (joinedAirDF.groupBy('pickup_month', 'pickup_day', 'zip')
+                        .agg(f.count('pickup_day').alias('count_rides'), f.avg('categoryNumber').alias('avg_cat')))
+    
+    #win = Window.partitionBy("zip", "pickup_month").orderBy(f.desc("avg_cat", 'count_rides'))
+    #aggDF = aggDF.withColumn("row_num", f.row_number().over(win)).where("row_num >= 10")
+
+    return aggDF
+
 if __name__ == '__main__':
     spark = SparkSession.builder.appName("OptimizeJob").getOrCreate()
     inputDF = (spark
@@ -88,13 +97,16 @@ if __name__ == '__main__':
               .json(taxiPath)
               .withColumn("pickup_month", f.date_format("pickup_datetime", "yyyyMM")))
     write_sorted_parquet(inputDF)
-    taxi_data = get_taxi_df()
-    zip_mapping = get_zip_code_mapping_df()
-    airQualityDF = get_air_quality_df(zip_mapping)
-    hottest_days = calculate_hottest_days(taxi_data, airQualityDF)
+    taxiDF = get_taxi_df()
+    zipDF = get_zip_code_mapping_df()
+    airQualityDF = get_air_quality_df(zipDF)
+    airQualityDF.show()
+    aggedDF = calculate_hottest_days(taxiDF, airQualityDF, zipDF)
+    aggedDF.cache()
+    print(aggedDF.count())
     
     # Probably want to change this, just testing for now
-    save_data(taxi_data, "s3://data-scale-oreilly/data/EMR/taxi_data", "parquet", "overwrite")
-    save_data(zip_mapping, "s3://data-scale-oreilly/data/EMR/zip_mapping", "parquet", "overwrite")
-    save_data(hottest_days, "s3://data-scale-oreilly/data/EMR/hottest_days", "csv", "overwrite")
+    save_data(taxiDF, "s3://data-scale-oreilly/data/EMR/taxi_data", "parquet", "overwrite")
+    save_data(zipDF, "s3://data-scale-oreilly/data/EMR/zip_mapping", "parquet", "overwrite")
+    save_data(aggedDF, "s3://data-scale-oreilly/data/EMR/hottest_days", "csv", "overwrite")
     
